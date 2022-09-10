d5d6a018cfff893d7415e608c72d03b9491765c1
PyPranav <pranavsunil2103@gmail.com>
2022-09-10 11:38:00 +0530

    created database

--- Class_Attendance_Tracker/main/models.py
+++ Class_Attendance_Tracker/main/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class ClassName(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=5)
+    def __str__(self):
+        return self.name
+
+class Subject(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    subjectName = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.subjectName
+
+class Student(models.Model):
+    classname = models.ForeignKey(ClassName, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rollno = models.CharField(max_length=10)
+    firstname = models.CharField(max_length=200)
+    lastname = models.CharField(max_length=200)
+    netAttendance = models.IntegerField()
+    
+    def __str__(self):
+        return self.firstname+" "+self.lastname
+
+class Attendance(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    datetime = models.DateTimeField()
+    ispresent = models.BooleanField()
+
+    def __str__(self):
+        return self.ispresent
