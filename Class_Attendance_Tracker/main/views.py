7b34f5d0f870bf2ae6b5a151d5a6201dbfb10b77
PyPranav <pranavsunil2103@gmail.com>
2022-10-16 21:39:15 +0530

    Create class page finished

--- Class_Attendance_Tracker/main/views.py
+++ Class_Attendance_Tracker/main/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth import login, logout, authenticate
@@ -7,7 +6,8 @@ from django.contrib.auth.decorators import login_required
 from .models import *
 from datetime import datetime
 
-#_________________________________________Login Logout and Regitering User___________________________________________
+#_________________________________________Login Logout and Registering User___________________________________________
+
 def loginPage(request):
     if request.user.is_authenticated:
         return redirect('home')
@@ -45,12 +45,47 @@ def logoutPage(request):
 @login_required(login_url='login')
 def home(request):
     context={}
+    latest=request.user.attendance_set.last()
+    context["clsName"]=latest.student.classname.name
+    context['dt']=latest.datetime.strftime("%d-%m-%Y, %H:%M")
+    context['subj']=latest.subject
+    # print(clsName,dt,subj)
     return render(request, "home.html", context)
 
 @login_required(login_url="login")
 def createClass(request):
-    context={}
+    context={"takeNewStudentDetails":False}
+
+    if request.method=="GET" and request.GET:
+        req = request.GET
+        if request.user.classname_set.filter(name = req.get('year')+'_'+req.get('className')):
+            messages.info(request,"Class already exists!")
+            return render(request, 'createClass.html', context )
+        if req.get('strength').isdigit() and int(req.get('strength'))<=999 and int(req.get('strength'))>=1:
+            context["className"] = req.get('year')+'_'+req.get('className')
+            context['rnge'] = map(lambda x: (3-len(str(x)))*"0"+str(x) ,range(1,int(req.get('strength'))+1))
+            context["takeNewStudentDetails"] = True
+        else:
+            messages.info(request,"Please enter a valid strength of class!")
+
+    if request.method=="POST":
+        reqP = request.POST
+        reqG = request.GET
+        clsName = request.user.classname_set.create(name=reqG.get('year')+'_'+reqG.get('className'))
+        request.user.save()
+        strength = int(reqG.get("strength"))
+        for i in range(1,strength+1):
+            i = str(i)
+            i = (3-len(i))*"0"+i
+            fname = reqP.get('firstName_'+i)
+            lname = reqP.get('lastName_'+i)
+            rollno = clsName.name+i
+            clsName.student_set.create(user=request.user, rollno=rollno, firstname=fname, lastname=lname)
+        clsName.save()
+        messages.success(request, f"New class {clsName.name} was created")
+        return redirect("home")
     return render(request, 'createClass.html', context )
+
 
 @login_required(login_url="login")
 def takeAttendance(request):
@@ -69,7 +104,7 @@ def takeAttendance(request):
         return redirect('home')
         
 
-    if request.method == "GET" and request.GET and "className" in request.GET:
+    if request.method == "GET" and request.GET and "className" in request.GET and "subName" in request.GET:
         print(request.GET)
         context["takeAttendanceDetails"]=False
         context["className"] = request.GET.get('className')
